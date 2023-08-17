b4aa66aaeb61e00bf938c098bef4cec7333bad76
nazarchykur <nazarchykur@gmail.com>
2023-08-17 14:00:03 +0300

    added example of how load balancer work

--- product-service/src/main/java/com/example/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/example/productservice/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.example.productservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/products")
 public class ProductController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping
     public String getProducts() {
-        return "Get Request: Get All Products";
+        return "Get Request: Get All Products " + environment.getProperty("local.server.port");
     }
 
     @PostMapping
