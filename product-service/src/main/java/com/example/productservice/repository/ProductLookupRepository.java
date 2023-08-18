a014e9209ba52c61a5131e92710f083af490edac
nazarchykur <nazarchykur@gmail.com>
2023-08-18 16:20:50 +0300

    added ProductLookupRepository

---
+++ product-service/src/main/java/com/example/productservice/repository/ProductLookupRepository.java
@@ -0,0 +1,10 @@
+package com.example.productservice.repository;
+
+import com.example.productservice.entity.ProductLookup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductLookupRepository extends JpaRepository<ProductLookup, String> {
+    ProductLookup findByProductIdOrTitle(String productId, String title);
+}
