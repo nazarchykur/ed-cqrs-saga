98723e0a53b965dac5cd16eee83feade0b065e3f
nazarchykur <nazarchykur@gmail.com>
2023-08-18 17:08:14 +0300

    Persisted information into a ProductLookup table

--- product-service/src/main/java/com/example/productservice/command/ProductLookupEventsHandler.java
+++ product-service/src/main/java/com/example/productservice/command/ProductLookupEventsHandler.java
@@ -1,16 +1,23 @@
 package com.example.productservice.command;
 
+import com.example.productservice.entity.ProductLookup;
 import com.example.productservice.event.ProductCreatedEvent;
+import com.example.productservice.repository.ProductLookupRepository;
+import lombok.RequiredArgsConstructor;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.stereotype.Component;
 
 @Component
 @ProcessingGroup("product-group")
+@RequiredArgsConstructor
 public class ProductLookupEventsHandler {
+
+    private final ProductLookupRepository productLookupRepository;
 
     @EventHandler
     public void on(ProductCreatedEvent event) {
-
+        ProductLookup productLookup = new ProductLookup(event.getProductId(), event.getTitle());
+        productLookupRepository.save(productLookup);
     }
 }
--- product-service/src/main/java/com/example/productservice/entity/ProductLookup.java
+++ product-service/src/main/java/com/example/productservice/entity/ProductLookup.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
@@ -15,6 +17,8 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "product_lookup")
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @ToString
