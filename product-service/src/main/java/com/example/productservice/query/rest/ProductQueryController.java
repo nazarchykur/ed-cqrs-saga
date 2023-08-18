1010491eccd37d51ea7ec0f5cd77063de6c9ac78
nazarchykur <nazarchykur@gmail.com>
2023-08-18 13:50:30 +0300

    added ProductQueryController and ProductRestModel

---
+++ product-service/src/main/java/com/example/productservice/query/rest/ProductQueryController.java
@@ -0,0 +1,27 @@
+package com.example.productservice.query.rest;
+
+import com.example.productservice.query.FindProductQuery;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
+import org.axonframework.queryhandling.QueryGateway;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductQueryController {
+    private QueryGateway queryGateway;
+
+    public ProductQueryController(QueryGateway queryGateway) {
+        this.queryGateway = queryGateway;
+    }
+
+    @GetMapping
+    public List<ProductRestModel> getProducts(){
+        FindProductQuery findProductQuery = new FindProductQuery();
+        return queryGateway.query(findProductQuery, ResponseTypes.multipleInstancesOf(ProductRestModel.class)).join();
+    }
+
+}
