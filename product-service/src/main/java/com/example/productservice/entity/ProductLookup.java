26fe22ee218aed1d073f009b655a056cff21d153
nazarchykur <nazarchykur@gmail.com>
2023-08-18 16:19:26 +0300

    added ProductLookup entity

---
+++ product-service/src/main/java/com/example/productservice/entity/ProductLookup.java
@@ -0,0 +1,44 @@
+package com.example.productservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "product_lookup")
+@Getter
+@Setter
+@ToString
+public class ProductLookup {
+
+    @Id
+    private String productId;
+
+    @Column(unique = true)
+    private String title;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        ProductLookup that = (ProductLookup) o;
+        return getProductId() != null && Objects.equals(getProductId(), that.getProductId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
