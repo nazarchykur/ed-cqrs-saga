4373af7370dd885554b618f64600d7f8a6006d3a
nazarchykur <nazarchykur@gmail.com>
2023-08-22 12:17:01 +0300

    added example @ExceptionHandler from axonframework interceptors that can handle exceptions only within its class

--- product-service/src/main/java/com/example/productservice/query/ProductEventsHandler.java
+++ product-service/src/main/java/com/example/productservice/query/ProductEventsHandler.java
@@ -6,6 +6,7 @@ import com.example.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.messaging.interceptors.ExceptionHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
@@ -16,10 +17,26 @@ public class ProductEventsHandler {
 
     private final ProductRepository productRepository;
 
+    // this @ExceptionHandler from axonframework interceptors can handle any exception only within this class, and not globally
+    @ExceptionHandler(resultType = Exception.class)
+    public void handle(Exception exception) {
+        // log error message
+    }
+
+    @ExceptionHandler(resultType = IllegalArgumentException.class)
+    public void handle(IllegalArgumentException exception) {
+        // log error message
+    }
+
     @EventHandler
     public void on(ProductCreatedEvent event) {
         Product product = new Product();
         BeanUtils.copyProperties(event, product);
-        productRepository.save(product);
+        try {
+            productRepository.save(product);
+        } catch (IllegalArgumentException e) {
+            // log error message
+            e.printStackTrace();
+        }
     }
 }
