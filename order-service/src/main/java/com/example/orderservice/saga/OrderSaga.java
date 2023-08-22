8ccb557c5c116797103a1c026d9a9dc410edb4ae
nazarchykur <nazarchykur@gmail.com>
2023-08-22 17:17:10 +0300

    added OrderSaga class with annotation

---
+++ order-service/src/main/java/com/example/orderservice/saga/OrderSaga.java
@@ -0,0 +1,20 @@
+package com.example.orderservice.saga;
+
+import com.example.orderservice.event.OrderCreatedEvent;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.modelling.saga.SagaEventHandler;
+import org.axonframework.modelling.saga.StartSaga;
+import org.axonframework.spring.stereotype.Saga;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Saga
+public class OrderSaga {
+    @Autowired
+    private transient CommandGateway commandGateway;
+
+    @StartSaga
+    @SagaEventHandler(associationProperty = "orderId")
+    public void handle(OrderCreatedEvent event) {
+
+    }
+}
