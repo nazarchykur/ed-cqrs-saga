0e4c44a03d5e546b8e2747d1f693f5db5cf01d28
nazarchykur <nazarchykur@gmail.com>
2023-08-23 11:21:21 +0300

    added changes to root folder to use core module as a dependency in order-service module and use ReserveProductCommand in SagaEventHandler

--- order-service/src/main/java/com/example/orderservice/saga/OrderSaga.java
+++ order-service/src/main/java/com/example/orderservice/saga/OrderSaga.java
@@ -1,5 +1,6 @@
 package com.example.orderservice.saga;
 
+import com.example.core.command.ReserveProductCommand;
 import com.example.orderservice.event.OrderCreatedEvent;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.modelling.saga.SagaEventHandler;
@@ -15,6 +16,29 @@ public class OrderSaga {
     @StartSaga
     @SagaEventHandler(associationProperty = "orderId")
     public void handle(OrderCreatedEvent event) {
+        ReserveProductCommand reserveProductCommand = ReserveProductCommand.builder()
+                .orderId(event.getOrderId())
+                .productId(event.getProductId())
+                .quantity(event.getQuantity())
+                .userId(event.getUserId())
+                .build();
+
+        // use lambda
+        commandGateway.send(reserveProductCommand, (commandMessage, commandResultMessage) -> {
+            if (commandResultMessage.isExceptional()) {
+                // start a compensation transaction
+            }
+        });
+
+        // use Anonymous class
+//        commandGateway.send(reserveProductCommand, new CommandCallback<ReserveProductCommand, Object>() {
+//            @Override
+//            public void onResult(@Nonnull CommandMessage<? extends ReserveProductCommand> commandMessage, @Nonnull CommandResultMessage<?> commandResultMessage) {
+//                if (commandResultMessage.isExceptional()) {
+//                    // start a compensation transaction
+//                }
+//            }
+//        });
 
     }
 }
