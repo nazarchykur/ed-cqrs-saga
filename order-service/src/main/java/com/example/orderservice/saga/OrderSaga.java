6772a299b9b30f04d4e3a659b23e010b9c8fcbb3
nazarchykur <nazarchykur@gmail.com>
2023-08-23 15:32:13 +0300

    added an example of fetching UserPaymantDetails

--- order-service/src/main/java/com/example/orderservice/saga/OrderSaga.java
+++ order-service/src/main/java/com/example/orderservice/saga/OrderSaga.java
@@ -2,19 +2,27 @@ package com.example.orderservice.saga;
 
 import com.example.core.command.ReserveProductCommand;
 import com.example.core.event.ProductReservedEvent;
+import com.example.core.model.User;
+import com.example.core.query.FetchUserPaymentDetailsQuery;
 import com.example.orderservice.event.OrderCreatedEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.modelling.saga.SagaEventHandler;
 import org.axonframework.modelling.saga.StartSaga;
+import org.axonframework.queryhandling.QueryGateway;
 import org.axonframework.spring.stereotype.Saga;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Slf4j
 @Saga
 public class OrderSaga {
+
     @Autowired
     private transient CommandGateway commandGateway;
+
+    @Autowired
+    private transient QueryGateway queryGateway;
 
     @StartSaga
     @SagaEventHandler(associationProperty = "orderId")
@@ -39,5 +47,22 @@ public class OrderSaga {
     public void handle(ProductReservedEvent productReservedEvent) {
         // process user payment
         log.info("ProductReservedEvent is called for orderId: " + productReservedEvent.getOrderId() + " and productId: " + productReservedEvent.getProductId());
+
+        FetchUserPaymentDetailsQuery fetchUserPaymentDetailsQuery = new FetchUserPaymentDetailsQuery(productReservedEvent.getUserId())
+
+        User userPaymentDetails;
+        try {
+            userPaymentDetails = queryGateway.query(fetchUserPaymentDetailsQuery, ResponseTypes.instanceOf(User.class)).join();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            // start a compensation transaction
+            return;
+        }
+        if (userPaymentDetails == null) {
+            // start a compensation transaction
+            return;
+        }
+
+        log.info("Successfully fetched user payment details for user: " + userPaymentDetails.getFirstName());
     }
 }
