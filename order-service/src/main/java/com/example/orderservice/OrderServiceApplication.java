bb0a57deffe3a5f39fac381f0d54a49ec975d798
nazarchykur <nazarchykur@gmail.com>
2023-08-25 10:52:20 +0300

    added a bean DeadlineManager

--- order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
@@ -1,8 +1,21 @@
 package com.example.orderservice;
 
+import org.axonframework.common.transaction.TransactionManager;
+import org.axonframework.config.Configuration;
+import org.axonframework.config.ConfigurationScopeAwareProvider;
+import org.axonframework.deadline.DeadlineManager;
+import org.axonframework.deadline.SimpleDeadlineManager;
+import org.axonframework.spring.messaging.unitofwork.SpringTransactionManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+
+@EnableDiscoveryClient
+@EnableTransactionManagement
 @SpringBootApplication
 public class OrderServiceApplication {
 
@@ -10,4 +23,16 @@ public class OrderServiceApplication {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
 
+    @Bean
+    public TransactionManager axonTransactionManager(PlatformTransactionManager platformTransactionManager) {
+        return new SpringTransactionManager(platformTransactionManager);
+    }
+
+    @Bean
+    public DeadlineManager deadlineManager(Configuration configuration, TransactionManager axonTransactionManager) {
+        return SimpleDeadlineManager.builder()
+                .scopeAwareProvider(new ConfigurationScopeAwareProvider(configuration))
+                .transactionManager(axonTransactionManager)
+                .build();
+    }
 }
