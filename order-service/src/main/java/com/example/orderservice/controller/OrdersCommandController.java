ca159d7cdb1a282d7a000ab90c675a3f9cfc0897
nazarchykur <nazarchykur@gmail.com>
2023-08-25 17:19:14 +0300

    added an example of sending a Subscription Query

--- order-service/src/main/java/com/example/orderservice/controller/OrdersCommandController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrdersCommandController.java
@@ -2,8 +2,14 @@ package com.example.orderservice.controller;
 
 import com.example.orderservice.command.CreateOrderCommand;
 import com.example.orderservice.command.OrderStatus;
+import com.example.orderservice.command.OrderSummary;
+import com.example.orderservice.query.FindOrderQuery;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
+import org.axonframework.queryhandling.QueryGateway;
+import org.axonframework.queryhandling.SubscriptionQueryResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,18 +23,32 @@ import java.util.UUID;
 public class OrdersCommandController {
 
     private final CommandGateway commandGateway;
+    private final QueryGateway queryGateway;
 
     @PostMapping
-    public String createOrder(@RequestBody CreateOrderCommand order) {
+    public OrderSummary createOrder(@Valid @RequestBody CreateOrderCommand order) {
+        String userId = "27b95829-4f3f-4ddf-8983-151ba010e35b";
+        String orderId = UUID.randomUUID().toString();
+
         CreateOrderCommand createOrderCommand = CreateOrderCommand.builder()
                 .orderId(UUID.randomUUID().toString())
-                .userId("27b95829-4f3f-4ddf-8983-151ba010e35b")
+                .userId(userId)
                 .productId(order.getProductId())
                 .quantity(order.getQuantity())
                 .addressId(order.getAddressId())
                 .orderStatus(OrderStatus.CREATED)
                 .build();
 
-        return commandGateway.sendAndWait(createOrderCommand);
+        SubscriptionQueryResult<OrderSummary, OrderSummary> queryResult =
+                queryGateway.subscriptionQuery(new FindOrderQuery(orderId),
+                        ResponseTypes.instanceOf(OrderSummary.class),
+                        ResponseTypes.instanceOf(OrderSummary.class));
+
+        try {
+            commandGateway.sendAndWait(createOrderCommand);
+            return queryResult.updates().blockFirst();
+        } finally {
+            queryResult.close();
+        }
     }
 }
